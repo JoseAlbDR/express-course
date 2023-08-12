7799d2cf517d61d6d78e14712cbe4d479f6bdca0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-12 16:49:21 +0200

    feat: http server up

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Express Tutorial')
+import http from "http";
+
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  console.log("User hit the server");
+  res.end("<h1>Home Page</h1>");
+});
+
+server.listen(port);
