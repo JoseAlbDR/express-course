9e50ce1ab7d9ed046879a2cb6039de4ef22862c0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 00:28:26 +0200

    feat: add new service getProduct

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,7 +1,16 @@
 import { products as productsData } from "../data/data";
 import { Product } from "../interfaces/types";
-const products: Product[] = productsData as Product[];
+const products: Product[] = productsData;
 
-export const getProducts = (): Product[] => {
+const getProducts = (): Product[] => {
   return products;
 };
+
+const getProduct = (id: number): Product | undefined => {
+  const product: Product | undefined = products.find(
+    (product) => product.id === id
+  );
+  return product;
+};
+
+export default { getProducts, getProduct };
