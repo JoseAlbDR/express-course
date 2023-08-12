eaf04b7bccbf544e938fa43b706296da10a65ef5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 00:28:49 +0200

    feat: add new route to /:productId to get a product

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,11 +1,26 @@
 import express from "express";
-import { getProducts } from "../services/productService";
+import productService from "../services/productService";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  const products = getProducts();
+  const products = productService.getProducts();
   res.json(products);
+});
+
+router.get("/:productId", (req, res) => {
+  const { productId } = req.params;
+
+  const product = productService.getProduct(+productId);
+
+  product
+    ? res.json(product)
+    : res
+        .status(404)
+        .send({
+          status: "FAILURE",
+          message: `No product with id ${productId}`,
+        });
 });
 
 export default router;
