833b4d3a655723d42d760aa6e22bfc7aa2d19b4a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-12 19:43:51 +0200

    feat: serving all files as statics

--- app.js
+++ app.js
@@ -8,24 +8,20 @@ const port = 5000;
 
 const app = express();
 app.use(express.static("./navbar-app"));
-// app.get
-app.get("/", (req, res) => {
-  res.sendFile(__dirname, "./navbar-app/index.html");
-});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname, "./navbar-app/index.html");
+//   adding to static assets
+//   SSR
+//
+// });
 
 app.get("/about", (req, res) => {
   res.status(200).send("<h1>About</h1>");
 });
 
-// app.all
 app.all("*", (req, res) => {
   res.status(404).send("<h1>Page not found</h1>");
 });
 
-// app.post
-// app.put
-// app.delete
-// app.use
-
-// app.listen
 app.listen(port);
