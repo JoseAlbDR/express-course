9f0cc802fb919a823b5a2d17cf098c19e6fac3f2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-12 18:40:57 +0200

    feat: /, /about and * routes with http status codes

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const port = 5000;
 
 const app = express();
 
+// app.get
 app.get("/", (req, res) => {
-  res.send("<h1>Express server.</h1>");
+  res.status(200).send("<h1>Home Page</h1>");
 });
 
+app.get("/about", (req, res) => {
+  res.status(200).send("<h1>About</h1>");
+});
+
+// app.all
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
+// app.post
+// app.put
+// app.delete
+// app.use
+
+// app.listen
 app.listen(port);
