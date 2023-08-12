c8d903e4c7f14431f61aec13b16d678d71ce9699
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-12 18:25:14 +0200

    feat: serve image, css and js files

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ import http from "http";
 import { readFileSync } from "fs";
 
 // get all files
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const css = readFileSync("./navbar-app/styles.css");
+const logo = readFileSync("./navbar-app/logo.svg");
+const script = readFileSync("./navbar-app/browser-app.js");
 
 const port = 5000;
 
@@ -14,6 +17,24 @@ const server = http.createServer((req, res) => {
       "content-type": "text/html",
     });
     res.write(homePage);
+    res.end();
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, {
+      "content-type": "image/svg+xml",
+    });
+    res.write(logo);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, {
+      "content-type": "text/javascript",
+    });
+    res.write(script);
+    res.end();
+  } else if (url === "/styles.css") {
+    res.writeHead(200, {
+      "content-type": "text/css",
+    });
+    res.write(css);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, {
