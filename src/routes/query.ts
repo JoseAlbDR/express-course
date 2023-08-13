9d5f1acf3086492f1b434c8ab73ad9d1a53d2b82
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 18:44:28 +0200

    feat: simple search and limit querys setup

--- src/routes/query.ts
+++ src/routes/query.ts
@@ -1,10 +1,29 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
+import { SearchParams } from "../interfaces/types";
+import { products } from "../data/data";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  console.log(req.query);
-  res.json(req.query);
+router.get("/", (req: SearchParams, res: Response) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) =>
+      product.name.startsWith(search)
+    );
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, +limit);
+  }
+
+  if (!sortedProducts.length) {
+    // res.status(200).send("No products found");
+    return res.status(200).json({ success: true, data: [] });
+  }
+
+  return res.status(200).json({ success: true, data: sortedProducts });
 });
 
 export default router;
