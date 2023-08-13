b295043dbf6e0c3943104bfc32ed5a14709009d2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 16:28:47 +0200

    feat: util errorHandler to handle custom errors

---
+++ src/utils/error.ts
@@ -0,0 +1,23 @@
+import { Response } from "express";
+import { CustomError } from "../interfaces/types";
+
+export const errorHandler = (error: unknown, res: Response) => {
+  if (
+    error &&
+    typeof error === "object" &&
+    "status" in error &&
+    "message" in error
+  ) {
+    // Check if the error has the properties of a CustomError
+    const customError = error as CustomError;
+    res
+      .status(customError.status)
+      .send({ status: "FAILED", data: { error: customError.message } });
+  } else {
+    // Handle other errors here
+    res.status(500).send({
+      status: "FAILED",
+      data: { error },
+    });
+  }
+};
