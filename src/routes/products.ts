7cdd9aec816e173690e4d2192ea12f76654954ce
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 15:40:31 +0200

    refactor: call questionController.getProduct in /:productId route

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,22 +1,10 @@
 import express from "express";
-import productService from "../services/productService";
 import questionController from "../controller/questionController";
 
 const router = express.Router();
 
 router.get("/", questionController.getProducts);
 
-router.get("/:productId", (req, res) => {
-  const { productId } = req.params;
-
-  const product = productService.getProduct(+productId);
-
-  product
-    ? res.json(product)
-    : res.status(404).send({
-        status: "FAILURE",
-        message: `No product with id ${productId}`,
-      });
-});
+router.get("/:productId", questionController.getProduct);
 
 export default router;
