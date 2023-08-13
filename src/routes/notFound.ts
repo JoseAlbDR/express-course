5143f0fafa1544d65419af6224ecb8cf0f2eb65d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 12:25:05 +0200

    feat: add 404 status to notFound route

--- src/routes/notFound.ts
+++ src/routes/notFound.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("*", (_req, res) => {
-  res.send("<h1>Page Not Found</h1><a href='/'>Back to Home</a>");
+  res.status(404).send("<h1>Page Not Found</h1><a href='/'>Back to Home</a>");
 });
 
 export default router;
