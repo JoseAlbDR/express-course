5b779923308e9ce245074a42a5f4fb8e1fbb5148
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 17:21:20 +0200

    feat: refactor questionController to productController, add new route to test more complex querys

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import questionController from "../controller/productController";
+import { Request, Response } from "express";
+import productController from "../controller/productController";
 
 const router = express.Router();
 
-router.get("/", questionController.getProducts);
+router.get("/", productController.getProducts);
 
-router.get("/:productId", questionController.getProduct);
+router.get("/:productId", productController.getProduct);
+
+router.get("/:productId/reviews/:reviewId", (req: Request, res: Response) => {
+  console.log(req.params);
+  res.send("Complex query");
+});
 
 export default router;
