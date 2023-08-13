d124752bcf856afe85369eb0491e5a25decc94a7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 18:57:54 +0200

    feat: middleware with next() example

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import productsRouter from "./src/routes/products";
-import homeRouter from "./src/routes/home";
+// import homeRouter from "./src/routes/home";
 import notFoundRouter from "./src/routes/notFound";
 import queryRouter from "./src/routes/query";
 
@@ -9,7 +9,23 @@ app.use(express.json());
 
 const PORT = 3000;
 
-app.use("/", homeRouter);
+// app.use("/", homeRouter);
+
+const logger = (req: Request, _res: Response, next: NextFunction) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+
+app.get("/", logger, (_req: Request, res: Response) => {
+  res.send("Home");
+});
+app.get("/about", logger, (_req: Request, res: Response) => {
+  res.send("About");
+});
+
 app.use("/api/products", productsRouter);
 app.use("/api/query", queryRouter);
 app.use("*", notFoundRouter);
