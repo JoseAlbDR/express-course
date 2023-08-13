57fca21c3794a4e6383343b5a95d1b88238a8523
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 12:40:36 +0200

    feat: add controller layer to questions end point

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,12 +1,10 @@
 import express from "express";
 import productService from "../services/productService";
+import questionController from "../controller/questionController";
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-  const products = productService.getProducts();
-  res.json(products);
-});
+router.get("/", questionController.getProducts);
 
 router.get("/:productId", (req, res) => {
   const { productId } = req.params;
@@ -15,12 +13,10 @@ router.get("/:productId", (req, res) => {
 
   product
     ? res.json(product)
-    : res
-        .status(404)
-        .send({
-          status: "FAILURE",
-          message: `No product with id ${productId}`,
-        });
+    : res.status(404).send({
+        status: "FAILURE",
+        message: `No product with id ${productId}`,
+      });
 });
 
 export default router;
